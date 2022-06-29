d4729ed9eed68b18e8672f558ee2408b56a385a2
Kurt Müllner <kurt.muellner@bluewin.ch>
2022-06-29 12:52:24 +0200

    Testfall auf false geändert.

--- Ueb11a_StrassenverkehrsamtTest/UnitTestPerson.cs
+++ Ueb11a_StrassenverkehrsamtTest/UnitTestPerson.cs
@@ -11,7 +11,7 @@ namespace Ueb11a_StrassenverkehrsamtTest
             Person p = new Person();
 
             // not finished
-            Assert.IsTrue(true);
+            Assert.IsTrue(false);
         }
     }
 }
